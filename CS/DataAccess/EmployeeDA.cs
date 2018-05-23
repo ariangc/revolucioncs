490181c841ad9024f75abde6008ac9e5defb0a41
sergior96 <sergiorm96@gmail.com>
2018-05-22 20:34:44 -0500

    sergioR

--- CS/DataAccess/EmployeeDA.cs
+++ CS/DataAccess/EmployeeDA.cs
@@ -15,7 +15,9 @@ namespace DataAccess
 
         public string returnPassword()
         {
-
+            string cadena = "Server=quilla.lab.inf.pucp.edu.pe;" + "User=inf282g3; database=inf282g3;" +
+                "port=3306; password=H9u1oC; SslMode=none;" + "";
+            
             return "1";
         }
     }
